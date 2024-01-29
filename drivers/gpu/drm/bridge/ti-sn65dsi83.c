e58913c1943bff523af26ef956eb4aea708c86a3
Primoz Fiser <primoz.fiser@norik.com>
2024-01-29 08:31:03 -0800

    HACK: drm/bridge: sn65dsi83: Remove MIPI_DSI_MODE_VIDEO_BURST flag
    Jacinto SoC's internal DSI bridge driver 'cdns-dsi' does not yet support
    MIPI_DSI_MODE_VIDEO_BURST mode. Thus remove the flag to be able to
    attach external sn65dsi83 DSI to LVDS bridge to it.
    
    Signed-off-by: Primoz Fiser <primoz.fiser@norik.com>
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>
    Signed-off-by: Dominik Haller <d.haller@phytec.de>

--- drivers/gpu/drm/bridge/ti-sn65dsi83.c
+++ drivers/gpu/drm/bridge/ti-sn65dsi83.c
@@ -655,7 +655,7 @@ static int sn65dsi83_host_attach(struct sn65dsi83 *ctx)
 
 	dsi->lanes = dsi_lanes;
 	dsi->format = MIPI_DSI_FMT_RGB888;
-	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST |
+	dsi->mode_flags = MIPI_DSI_MODE_VIDEO |
 			  MIPI_DSI_MODE_VIDEO_NO_HFP | MIPI_DSI_MODE_VIDEO_NO_HBP |
 			  MIPI_DSI_MODE_VIDEO_NO_HSA | MIPI_DSI_MODE_NO_EOT_PACKET;
 
