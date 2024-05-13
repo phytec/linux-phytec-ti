8d205bdef6b6723b1297c4ad858965bd93e8398e
Fabian Godehardt <fg@emlix.com>
2024-05-13 16:14:00 +0200

    rtc: rv3029: Add device tree properties for backup switchover mode
    The backup switchover mode can be setup for direct (DSM), level (LSM) or
    disabled mode via device tree properties enable-direct-switching-mode and
    enable-level-switching-mode. Default is disabled backup switchover.
    
    The application manual Rev. 1.1 from August 2019 describes in the BSM
    register description that access to the EEPROM is valid only if switchover
    mode is disabled. So the EEPROM nvmem region will be registered only if
    switchover mode is disabled.
    
    Signed-off-by: Fabian Godehardt <fg@emlix.com>
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

--- drivers/rtc/rtc-rv3028.c
+++ drivers/rtc/rtc-rv3028.c
@@ -85,6 +85,10 @@
 
 #define RV3028_BACKUP_BSM_DSM		0x1
 #define RV3028_BACKUP_BSM_LSM		0x3
+#define RV3028_BACKUP_BSM_DISABLE_DFLT	0x0
+#define RV3028_BACKUP_BSM_DISABLE	BIT(3)
+#define RV3028_BACKUP_BSM_DSM_ENABLE	BIT(2)
+#define RV3028_BACKUP_BSM_LSM_ENABLE	(BIT(2) | BIT(3))
 
 #define OFFSET_STEP_PPT			953674
 
@@ -858,7 +862,8 @@ static const struct regmap_config regmap_config = {
 static int rv3028_probe(struct i2c_client *client)
 {
 	struct rv3028_data *rv3028;
-	int ret, status;
+	int ret, status, delay = 0;
+	unsigned int mode, mode_old;
 	u32 ohms;
 	struct nvmem_config nvmem_cfg = {
 		.name = "rv3028_nvram",
@@ -954,6 +959,32 @@ static int rv3028_probe(struct i2c_client *client)
 		} else {
 			dev_warn(&client->dev, "invalid trickle resistor value\n");
 		}
+	}
+
+	/* setup switching mode - write to EEPROM only if changed */
+	ret = regmap_read(rv3028->regmap, RV3028_BACKUP, &mode_old);
+	if (ret)
+		return ret;
+	mode_old &= RV3028_BACKUP_BSM;
+
+	if (device_property_present(&client->dev,
+				    "enable-direct-switching-mode")) {
+		mode = RV3028_BACKUP_BSM_DSM_ENABLE;
+		delay = 2;
+	} else if (device_property_present(&client->dev,
+					   "enable-level-switching-mode")) {
+		mode = RV3028_BACKUP_BSM_LSM_ENABLE;
+		delay = 16;
+	} else
+		mode = RV3028_BACKUP_BSM_DISABLE_DFLT;
+
+	if (mode != mode_old) {
+		ret = regmap_update_bits(rv3028->regmap, RV3028_BACKUP,
+					 RV3028_BACKUP_BSM, mode);
+		if (ret)
+			return ret;
+		if (delay)
+			mdelay(delay); /* wait for switchover */
 	}
 
 	ret = rtc_add_group(rv3028->rtc, &rv3028_attr_group);
