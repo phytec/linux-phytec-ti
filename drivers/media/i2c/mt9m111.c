aee26bae3d4ac85fa18e19df032d819459f64889
Russell Robinson <rrobinson@phytec.com>
2026-06-05 10:14:00 +0200

    media: i2c: mt9m111: Support for optional reset GPIO
    Add support for optional reset GPIO.
    
    Signed-off-by: Russell Robinson <rrobinson@phytec.com>
    (fix hunk offset conflict, commit msg style improvements)
    Signed-off-by: Primoz Fiser <primoz.fiser@norik.com>
    Tested-by: Nathan Morrisson <nmorrisson@phytec.com>
    (cherry picked from commit e78a6827ced6ad1adcaab0e9ee2e7c011f9f60b0)
    Signed-off-by: Florijan Plohl <florijan.plohl@norik.com>
    Signed-off-by: Wadim Egorov <w.egorov@phytec.de>

--- drivers/media/i2c/mt9m111.c
+++ drivers/media/i2c/mt9m111.c
@@ -232,6 +232,7 @@ struct mt9m111 {
 	struct mt9m111_context *ctx;
 	struct v4l2_rect rect;	/* cropping rectangle */
 	struct clk *clk;
+	struct gpio_desc *reset;
 	unsigned int width;	/* output */
 	unsigned int height;	/* sizes */
 	struct v4l2_fract frame_interval;
@@ -970,6 +971,12 @@ static int mt9m111_power_on(struct mt9m111 *mt9m111)
 	struct i2c_client *client = v4l2_get_subdevdata(&mt9m111->subdev);
 	int ret;
 
+	/* Ensure RESET_BAR is active */
+	if (mt9m111->reset) {
+		gpiod_set_value(mt9m111->reset, 1);
+		usleep_range(1000, 2000);
+	}
+
 	ret = clk_prepare_enable(mt9m111->clk);
 	if (ret < 0)
 		return ret;
@@ -977,6 +984,12 @@ static int mt9m111_power_on(struct mt9m111 *mt9m111)
 	ret = regulator_enable(mt9m111->regulator);
 	if (ret < 0)
 		goto out_clk_disable;
+
+	/* Set RESET_BAR high (de-assert) */
+	if (mt9m111->reset) {
+		gpiod_set_value(mt9m111->reset, 0);
+		usleep_range(1000, 2000);
+	}
 
 	ret = mt9m111_resume(mt9m111);
 	if (ret < 0)
@@ -999,6 +1012,13 @@ static void mt9m111_power_off(struct mt9m111 *mt9m111)
 {
 	mt9m111_suspend(mt9m111);
 	regulator_disable(mt9m111->regulator);
+
+	/* Ensure RESET_BAR is active */
+	if (mt9m111->reset) {
+		gpiod_set_value(mt9m111->reset, 1);
+		usleep_range(1000, 2000);
+	}
+
 	clk_disable_unprepare(mt9m111->clk);
 }
 
@@ -1283,6 +1303,9 @@ static int mt9m111_probe(struct i2c_client *client)
 	mt9m111->clk = devm_clk_get(&client->dev, "mclk");
 	if (IS_ERR(mt9m111->clk))
 		return PTR_ERR(mt9m111->clk);
+
+	mt9m111->reset = devm_gpiod_get_optional(&client->dev, "reset",
+						 GPIOD_OUT_LOW);
 
 	if (mt9m111->clk) {
 		if (!of_property_read_u32(client->dev.of_node,
